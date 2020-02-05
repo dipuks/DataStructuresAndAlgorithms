2f18533c9e92426bffed69d714045f6a2d62b899
Dipu Krishnan <dipuks@gmail.com>
2020-02-04 21:28:34 -0600

    Add project files.

---
+++ DataStructuresAndAlgorithms.cpp
@@ -0,0 +1,15 @@
+// DataStructuresAndAlgorithms.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "EssentialConcepts.h"
+
+int main()
+{
+    EssentialConcepts ec;
+    ec.initStruct();
+
+
+
+    system("Pause");
+}
---
+++ EssentialConcepts.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define SIZE 10
+
+class EssentialConcepts
+{
+	int arr[SIZE];
+
+	struct MyStruct {
+		int age;
+		string name;
+		int id;
+	};
+
+public:
+
+	//Integer Array Initialization using random values.
+	//And the values are printed into the console window.
+	void initArray()
+	{
+		//Array being initialized with random values.
+		for (int i = 0; i < SIZE; i++)
+		{
+			arr[i] = rand();
+		}
+
+		//Array values are being printed.
+		for (int x : arr)
+		{
+			cout << "Array = " << x << endl;
+		}
+	}
+
+	//Struct initialization and printing it's values.
+	void initStruct()
+	{
+		//Struct being initialized
+		MyStruct ms;
+		ms.age = 23;
+		ms.id = 12345;
+		ms.name = "Robin";
+		
+		cout << "Age is " << ms.age << endl;
+		cout << "ID is " << ms.id << endl;
+		cout << "Name is " << ms.name << endl;
+
+		MyStruct ms1 = {35, "John", 7890};
+		cout << "Age is " << ms1.age << endl;
+		cout << "ID is " << ms1.id << endl;
+		cout << "Name is " << ms1.name << endl;
+	}
+
+};
+
